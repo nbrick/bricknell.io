a792999a5a1578b29138596115c46b66d8d78e89
Nic Bricknell <nebricks@gmail.com>
2016-04-02 11:58:51 +0100

    Make the blog index page slightly nicer

--- Nbrick.hs
+++ Nbrick.hs
@@ -13,8 +13,9 @@ renderBlog :: [(String, String)] -> Maybe [Node]
 renderBlog [] = -- Index.
   Just [ h1 [ text "some posts for you" ]
        , ul $ map -- TODO: Sort by date.
-                (\slug -> [ link ("blog?post=" ++ slug) slug ])
-                (Map.keys blogPosts)
+                (\(slug, post)
+                   -> [ link ("blog?post=" ++ slug) (bPostTitle post) ])
+                (Map.assocs blogPosts)
        ]
 renderBlog [("post", slug)] =
   let post = Map.lookup slug blogPosts
@@ -28,5 +29,10 @@ blogPosts = Map.fromList
       BlogPost "My first blog post" "2016-04-02"
         [ p [ text "some content" ]
         ]
+    )
+  , ( "another-post",
+      BlogPost "I sure do love to blog" "2016-04-03"
+       [ p [ text "fantastic content" ]
+       ]
     )
   ]
