9f19aebf7fc228ec7edbb12ccb2c33fcdcff75b6
Nic Bricknell <nebricks@gmail.com>
2016-04-02 23:45:12 +0100

    Refactor more things into the routing table

--- Website.hs
+++ Website.hs
@@ -3,9 +3,10 @@ module Website where
 import qualified Data.Map.Strict as Map
 import DOM
 
-route ""     []     = Just $ Document index
-route "blog" params = Document <$> renderBlog params
-route _      _      = Nothing
+route ""     []               = Just $ Document index
+route "blog" []               = Just $ Document postIndex
+route "blog" [("post", slug)] = Document <$> renderPost slug
+route _      _                = Nothing
 
 nav = ul [ [ link "/" "bricknell.io" ]
          , [ link "/blog" "blog" ]
@@ -16,35 +17,33 @@ index = [ nav
         , p [ text "see ", link "blog" "a blog" ]
         ]
 
-data BlogPost = BlogPost
-  { bPostTitle   :: String
-  , bPostDate    :: String
-  , bPostContent :: [Node]
+data Post = Post
+  { postTitle   :: String
+  , postDate    :: String
+  , postContent :: [Node]
   }
 
-renderBlog :: [(String, String)] -> Maybe [Node]
-renderBlog [] = -- Index.
-  Just [ nav
-       , h1 [ text "some posts for you" ]
-       , ul $ map -- TODO: Sort by date.
-                (\(slug, post)
-                   -> [ link ("?post=" ++ slug) (bPostTitle post) ])
-                (Map.assocs blogPosts)
-       ]
-renderBlog [("post", slug)] =
-  fmap
-    (\p -> [ nav, h1 [ text $ bPostTitle p ] ] ++ bPostContent p)
-    (Map.lookup slug blogPosts)
-renderBlog _ = Nothing
+postIndex = [ nav
+            , h1 [ text "some posts for you" ]
+            , ul $ map -- TODO: Sort by date.
+                     (\(slug, post)
+                        -> [ link ("?post=" ++ slug) (postTitle post) ])
+                     (Map.assocs posts)
+            ]
 
-blogPosts = Map.fromList
+renderPost slug =
+  fmap
+    (\p -> [ nav, h1 [ text $ postTitle p ] ] ++ postContent p)
+    (Map.lookup slug posts)
+
+posts = Map.fromList
   [ ( "my-first-blog-post",
-      BlogPost "My first blog post" "2016-04-02"
+      Post "My first blog post" "2016-04-02" -- TODO: Proper datetime.
         [ p [ text "some content" ]
         ]
     )
   , ( "another-post",
-      BlogPost "I sure do love to blog" "2016-04-03"
+      Post "I sure do love to blog" "2016-04-03"
        [ p [ text "fantastic content" ]
        ]
     )
