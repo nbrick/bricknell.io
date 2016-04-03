aba4997eeff9684273100e8be644af4167aee6eb
Nic Bricknell <nebricks@gmail.com>
2016-04-03 13:06:47 +0100

    Refactor Server.hs

--- Server.hs
+++ Server.hs
@@ -4,40 +4,30 @@ import Network.HTTP.Server
 import Network.URL
 import ContentType
 
-standardHeaders msg msgType =
-  [ Header HdrContentLength (show $ length msg)
+headers message contentType =
+  [ Header HdrContentLength (show $ length message)
   , Header HdrContentEncoding "UTF-8"
-  , Header HdrContentEncoding msgType
+  , Header HdrContentEncoding contentType
   ]
 
-responseWith (Just (Html document)) = do
-  message <- display $ Html document
+responseWith (Just content) = do
+  message <- display content
   return Response { rspCode = (2,0,0)
                   , rspBody = message
-                  , rspHeaders = standardHeaders message "text/html"
-                  , rspReason = "Because you're awesome! :)"
+                  , rspHeaders =
+                      headers message $ case content of
+                                          Html _       -> "text/html"
+                                          Stylesheet _ -> "text/css"
+                                          Textfile _   -> "text/plain"
+                  , rspReason = "OK"
                   }
-responseWith (Just (Stylesheet filename)) = do
-  message <- display $ Stylesheet filename
-  return Response { rspCode = (2,0,0)
-                  , rspBody = message
-                  , rspHeaders = standardHeaders message "text/css"
-                  , rspReason = "Because you're stylish! ;)"
-                  }
-responseWith (Just (Textfile filename)) = do
-  message <- display $ Textfile filename
-  return Response { rspCode = (2,0,0)
-                  , rspBody = message
-                  , rspHeaders = standardHeaders message "text/plain"
-                  , rspReason = "Because you asked for it!"
-                  }
-responseWith Nothing = return $
+responseWith Nothing = do
   let message = "404 :("
-    in Response { rspCode = (4,0,4)
-                , rspBody = message
-                , rspHeaders = standardHeaders message "text/plain"
-                , rspReason = "No content for that query!"
-                }
+  return Response { rspCode = (4,0,4)
+                  , rspBody = message
+                  , rspHeaders = headers message "text/plain"
+                  , rspReason = "Not found."
+                  }
 
 handleWith route addr url req =
   responseWith $ route (url_path url) (url_params url)
